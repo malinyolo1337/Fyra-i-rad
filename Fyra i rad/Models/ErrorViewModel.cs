2184197b99b812f911bc3ba6d20d2f3618e24cd2
malinyolo1337 <malinjakobssn@gmail.com>
2025-10-08 15:05:37 +0200

    Add project files.

---
+++ Fyra i rad/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fyra_i_rad.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Fyra i rad/Models/SpelModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Fyra_i_rad.Models
+{
+    public class SpelModel
+    {
+        public int SpelID { get; set; } 
+    }
+}
