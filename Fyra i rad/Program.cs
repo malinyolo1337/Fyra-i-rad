01381e2c385aa1e1ce7dfd4b09f3b4746c11421c
malinyolo1337 <malinjakobssn@gmail.com>
2025-10-22 15:27:17 +0200

    ändrade routingen till login sidan, kommenterade bort alvas och la till min gamla version

--- Fyra i rad/Program.cs
+++ Fyra i rad/Program.cs
@@ -1,28 +1,66 @@
+//var builder = WebApplication.CreateBuilder(args);
+
+//// ?? Lägg till stöd för MVC (controllers och views)
+//builder.Services.AddControllersWithViews();
+
+//var app = builder.Build();
+
+//// ?? Felhantering och HTTPS
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//// ?? Standardroute: öppna startsida eller controller
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Spelar}/{action=Index}/{id?}");
+
+//app.Run();
 var builder = WebApplication.CreateBuilder(args);
 
-// ?? Lägg till stöd för MVC (controllers och views)
+// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
-// ?? Felhantering och HTTPS
+
+
+
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseSession();
 
-app.UseRouting();
+    app.UseRouting();
 
-app.UseAuthorization();
+    app.UseAuthorization();
+    app.UseCookiePolicy();
 
-// ?? Standardroute: öppna startsida eller controller
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Spelar}/{action=Index}/{id?}");
 
-app.Run();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Spelar}/{action=Index}/{id?}");
+
+
+    app.Run();
+
+
 
