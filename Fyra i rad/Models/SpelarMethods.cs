a78ec829647c74808df48ca43daac1fb65ab8e99
Alvakentta <alva.kentta@gmail.com>
2025-10-22 15:13:26 +0200

    sql connection

--- Fyra i rad/Models/SpelarMethods.cs
+++ Fyra i rad/Models/SpelarMethods.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
 namespace FyraIRad.Models
