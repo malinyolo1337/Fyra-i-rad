e6540399245de12b19b3936b0d194d1fe4a9e9e0
majahogdahl <majahogdahl10@gmail.com>
2025-10-27 12:10:18 +0100

    Lägg till Regler-sida + navbarlänk; flytta vy till Views/Home/Regler.cshtml

--- Fyra i rad/Controllers/HomeController.cs
+++ Fyra i rad/Controllers/HomeController.cs
@@ -28,5 +28,11 @@ namespace Fyra_i_rad.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Regler()
+{
+    return View();
+}
+
     }
 }
